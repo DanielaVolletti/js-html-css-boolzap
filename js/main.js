db2f6ef4469580e6dd87f4067ae7547a65cb124c
Daniela Maria Volletti <danielavolletti@gmail.com>
2020-04-08 11:12:35 +0200

    aggiunta risposta automatica dopo un secondo dall'invio del mio messaggio

--- js/main.js
+++ js/main.js
@@ -1,15 +1,29 @@
 $(document).ready(function(){
 
+  // salvo in variabile dove andrò ad aggiungere ulteriori messaggi
+  var contenitoreInviati = $('.contenitore-testo');
 
-  $('.invio').click(function(){
-    var contenitoreInviati = $('.contenitore-testo');
-    var msgInviato = $('.scrivi-msg input').val();
+  // salvo in una variabile il pulsante invio
+  var invio = $('.invio');
+
+  // salvo in una variabile cosa ho scritto nell'input
+  var msgScritto = $('.scrivi-msg input');
+
+  // al click salvo ciò che ha scritto l'utente e lo aggiungo sotto i msg inviati
+  invio.click(function(){
+    var msgInviato = msgScritto.val();
     console.log(msgInviato);
-    contenitoreInviati.append("<div class='mittente clear'>"+
-                      "<div class='msg-inviato'>" + msgInviato + "</div>"+
-                  "</div>");
+
+    contenitoreInviati.append("<div class='mittente clear'><div class='msg-inviato'>" + msgInviato + "</div></div>");
 
     $('.scrivi-msg input').val("");
+
+    // dopo un secondo appare la risposta
+    setTimeout(risposta, 1000);
+    function risposta() {
+      contenitoreInviati.append('<div class="destinatario clear"><div class="msg-ricevuto"> Ok </div></div>');
+    }
+    
   })
 
 });
